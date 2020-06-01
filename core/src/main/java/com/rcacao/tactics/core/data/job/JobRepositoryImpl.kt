1b5e79b73c03c784049cc4bf9fa95ea71840645b
renan <rcacao10@gmail.com>
2020-06-01 08:12:30 -0300

    adjust Job repository

--- core/src/main/java/com/rcacao/tactics/core/data/job/JobRepositoryImpl.kt
+++ core/src/main/java/com/rcacao/tactics/core/data/job/JobRepositoryImpl.kt
@@ -4,5 +4,5 @@ import com.rcacao.tactics.core.data.Result
 import com.rcacao.tactics.core.data.job.datasource.JobDataSource
 
 class JobRepositoryImpl(private val jobDataSource: JobDataSource) : JobRepository {
-    override suspend fun getJob(): Result<Job> = Result.Success(jobDataSource.getInitialJob())
+    override suspend fun getJob(): Result<Job> = jobDataSource.getInitialJob()
 }
